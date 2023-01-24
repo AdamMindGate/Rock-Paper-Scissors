667425d93c04df4f958cbcba33b92f8c3fff3307
AdamMindGate <dirtyhovac@gmail.com>
2023-01-24 23:12:54 +0200

    code cleaned and enhanced

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-let comp_points = 0;
-let p_points = 0;
+let comp_points = 0,
+  p_points = 0;
 let btn = document.getElementsByClassName("btn");
 let computer = document.getElementById("computer");
 let player = document.getElementById("player");
 let score = document.getElementById("score");
 let result = document.getElementById("result");
 /*----------------------------------------------------------------*/
-let comp_move;
 function computer_play() {
-  //everytime the functions is called, it will generate new random number
-  comp_move = Math.floor(Math.random() * 3 + 1);
-  // the random number determine the next move for the computer
+  let comp_move = Math.floor(Math.random() * 3 + 1);
   if (comp_move === 1) {
     computer.setAttribute("src", "Media/Rock.png");
   } else if (comp_move === 2) {
@@ -18,67 +15,56 @@ function computer_play() {
   } else {
     computer.setAttribute("src", "Media/Scissors.png");
   }
-  // returns the computer move & console log for testing
-  return computer, comp_move, console.log(comp_move);
+  return comp_move;
 }
 /*----------------------------------------------------------------*/
 
-// determining the player move each time he clicks on a button
 btn[0].onclick = function () {
   player.setAttribute("src", "Media/Rock.png");
-  // runs the computer move after each click
-  computer_play();
-  // determine the winner
-  if (comp_move === 2) {
-    result.innerHTML = "Computer Won";
-    // console.log("you lost");
-    comp_points += 1;
-  } else if (comp_move !== 1) {
-    console.log("you won");
-    result.innerHTML = "Player Won";
-    p_points += 1;
-  } else {
+
+  let cpu = computer_play();
+  if (cpu == 1) {
     result.innerHTML = "Draw";
-    console.log("draw");
+  } else if (cpu == 2) {
+    comp_points++;
+    result.innerHTML = "Computer Won";
+  } else {
+    p_points++;
+    result.innerHTML = "Player Won";
   }
-  // update the score
+
   score.innerHTML = `${p_points} - ${comp_points}`;
 };
 
 btn[1].onclick = function () {
   player.setAttribute("src", "Media/Paper.png");
-  computer_play();
-  // determine the winner
-  if (comp_move === 3) {
-    result.innerHTML = "Computer Won";
-    // console.log("you lost");
-    comp_points += 1;
-  } else if (comp_move !== 2) {
-    result.innerHTML = "Player Won";
-    // console.log("you won");
-    p_points += 1;
-  } else {
+
+  let cpu = computer_play();
+  if (cpu == 2) {
     result.innerHTML = "Draw";
+  } else if (cpu == 3) {
+    comp_points++;
+    result.innerHTML = "Computer Won";
+  } else {
+    p_points++;
+    result.innerHTML = "Player Won";
   }
-  // update the score
+
   score.innerHTML = `${p_points} - ${comp_points}`;
 };
 
 btn[2].onclick = function () {
   player.setAttribute("src", "Media/Scissors.png");
-  computer_play();
-  // determine the winner
-  if (comp_move === 1) {
-    result.innerHTML = "Computer Won";
-    // console.log("you lost");
-    comp_points += 1;
-  } else if (comp_move !== 3) {
-    console.log("you won");
-    result.innerHTML = "Player Won";
-    p_points += 1;
-  } else {
+
+  let cpu = computer_play();
+  if (cpu == 3) {
     result.innerHTML = "Draw";
+  } else if (cpu == 1) {
+    comp_points++;
+    result.innerHTML = "Computer Won";
+  } else {
+    p_points++;
+    result.innerHTML = "Player Won";
   }
-  // update the score
   score.innerHTML = `${p_points} - ${comp_points}`;
 };
