443ac218f52ba2a65b7abb9411acae7f08c28558
AdamMindGate <dirtyhovac@gmail.com>
2024-03-26 21:25:50 +0200

    modified the html structure restyled the page modified the JS code

--- main.js
+++ main.js
@@ -1,70 +1,66 @@
 let comp_points = 0,
-  p_points = 0;
+    p_points = 0;
 let btn = document.getElementsByClassName("btn");
 let computer = document.getElementById("computer");
 let player = document.getElementById("player");
 let score = document.getElementById("score");
 let result = document.getElementById("result");
 /*----------------------------------------------------------------*/
+
 function computer_play() {
-  let comp_move = Math.floor(Math.random() * 3 + 1);
-  if (comp_move === 1) {
-    computer.setAttribute("src", "Media/Rock.png");
-  } else if (comp_move === 2) {
-    computer.setAttribute("src", "Media/Paper.png");
-  } else {
-    computer.setAttribute("src", "Media/Scissors.png");
-  }
-  return comp_move;
+    let comp_move = Math.floor(Math.random() * 3 + 1);
+    animation(computer, comp_move);
+    return comp_move;
 }
-/*----------------------------------------------------------------*/
 
+function animation(player, move) {
+    if (move === 1) {
+        player.setAttribute("src", "Media/Rock.png");
+    } else if (move === 2) {
+        player.setAttribute("src", "Media/Paper.png");
+    } else {
+        player.setAttribute("src", "Media/Scissors.png");
+    }
+}
+
+function winner(player_move, comp_move) {
+    if (comp_move == player_move) {
+        result.innerHTML = "Draw";
+    } else if (player_move == 2 && comp_move == 1) {
+        result.innerHTML = "Player Won";
+        p_points++;
+    } else if (player_move == 1 && comp_move == 3) {
+        result.innerHTML = "Player Won";
+        p_points++;
+    } else if (player_move == 3 && comp_move == 2) {
+        result.innerHTML = "Player Won";
+        p_points++;
+    } else if (player_move == 2 && comp_move == 1) {
+        result.innerHTML = "Player Won";
+        p_points++;
+    } else {
+        result.innerHTML = "Computer Won";
+        comp_points++;
+    }
+}
+// -------------------
 btn[0].onclick = function () {
-  player.setAttribute("src", "Media/Rock.png");
-
-  let cpu = computer_play();
-  if (cpu == 1) {
-    result.innerHTML = "Draw";
-  } else if (cpu == 2) {
-    comp_points++;
-    result.innerHTML = "Computer Won";
-  } else {
-    p_points++;
-    result.innerHTML = "Player Won";
-  }
-
-  score.innerHTML = `${p_points} - ${comp_points}`;
+    let player_move = 1;
+    animation(player, player_move);
+    winner(player_move, computer_play());
+    score.innerHTML = `${p_points} - ${comp_points}`;
 };
 
 btn[1].onclick = function () {
-  player.setAttribute("src", "Media/Paper.png");
-
-  let cpu = computer_play();
-  if (cpu == 2) {
-    result.innerHTML = "Draw";
-  } else if (cpu == 3) {
-    comp_points++;
-    result.innerHTML = "Computer Won";
-  } else {
-    p_points++;
-    result.innerHTML = "Player Won";
-  }
-
-  score.innerHTML = `${p_points} - ${comp_points}`;
+    let player_move = 2;
+    animation(player, player_move);
+    winner(player_move, computer_play());
+    score.innerHTML = `${p_points} - ${comp_points}`;
 };
 
 btn[2].onclick = function () {
-  player.setAttribute("src", "Media/Scissors.png");
-
-  let cpu = computer_play();
-  if (cpu == 3) {
-    result.innerHTML = "Draw";
-  } else if (cpu == 1) {
-    comp_points++;
-    result.innerHTML = "Computer Won";
-  } else {
-    p_points++;
-    result.innerHTML = "Player Won";
-  }
-  score.innerHTML = `${p_points} - ${comp_points}`;
+    let player_move = 3;
+    animation(player, player_move);
+    winner(player_move, computer_play());
+    score.innerHTML = `${p_points} - ${comp_points}`;
 };
